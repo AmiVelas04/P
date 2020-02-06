e8885f1184a88144a242ca7764645d3b41c8642f
AmiVelas04 <jose.velas04@outlook.com>
2020-02-06 17:44:18 -0600

    CAmbios de servidor

--- Core/configeneral.php
+++ Core/configeneral.php
@@ -1,5 +1,5 @@
 <?php
-const SERVERURL="http://localhost/p/";
+const SERVERURL="http://localhost/P/";
 Const proy="Centro de Estudios Avanzados P&C";
 date_default_timezone_set("America/Guatemala");
 ?>
